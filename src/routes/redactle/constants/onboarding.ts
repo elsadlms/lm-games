12b0d1055df534cad427001c305ca1fcad3d5006
Elsa <edelmas@sempo10009.glm.lan>
2023-09-12 16:39:03 +0200

    fix onboarding text

--- src/routes/redactle/constants/onboarding.ts
+++ src/routes/redactle/constants/onboarding.ts
@@ -1,6 +1,6 @@
 export const onboarding: string[] = [
   'Retrouvez la personnalité disparue qui se cache derrière cet article du <i>Monde</i>&nbsp;!',
-  'Essayez de deviner les mots utilisés dans l’article pour vous aider à identifier la bonne personne avec le moins d’essais possibles. Vous pouvez cliquer sur un mot masqué pour savoir le nombre de lettres qui le compose.',
+  'Essayez de deviner les mots utilisés dans l’article pour vous aider à identifier la bonne personne avec le moins d’essais possible. Vous pouvez cliquer sur un mot masqué pour savoir le nombre de lettres qui le composent.',
   'Pensez à votre stratégie&nbsp;! Est-ce une femme ou un homme&nbsp;? A quelle période a-t-elle vécu&nbsp;? Dans quel domaine s’est-elle illustrée&nbsp;?… Vous pouvez également utiliser les indices pour révéler jusqu’à trois mots dans l’article.',
   'Le jeu n’est pas limité en temps ou en nombre d’essais. Si vous en avez assez, vous pouvez toujours “Abandonner” pour révéler la solution, mais c’est moins drôle&nbsp;!',
   'Bonne chance&nbsp;!',
